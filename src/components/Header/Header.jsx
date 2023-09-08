39a47f1c907c3e13eae744af362e4879c40e8c95
Eugene Avdeev <ea.avdeev@gmail.com>
2023-09-08 12:25:19 +0300

    refactor(Header):logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,12 @@
 import React from 'react';
 import './Header.css';
 import Navigation from '../Navigation/Navigation.jsx';
-import logoDark from '../../images/logo_dark.svg';
+import logoIcon from '../../images/logo.svg';
 
 function Header() {
     return (
         <header className='header header_wrap'>
-            <img className='' src={logoDark} alt="logo type dark"/>
+            <img className='' src={logoIcon} alt="logo"/>
             {/*Шапка на главной странице, как и на других страницах, должна менять отображение,
             если пользователь авторизован или не авторизован.
             Такое поведение нужно сразу предусмотреть в вёрстке, даже несмотря на то,
