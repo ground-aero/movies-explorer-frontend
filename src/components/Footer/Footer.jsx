9a1b8bece98f42abcfb7ac2e4473a451172824e8
Eugene Avdeev <ea.avdeev@gmail.com>
2023-09-17 14:06:31 +0300

    build(components): footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,28 @@
 // Component Footer
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 function Footer () {
     return (
-        <footer className='footer footer_wrap'>
+        <footer className='footer content__section'>
 
-            <h3>FOOTER</h3>
+            <h3 className='footer__header'>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+            <hr className='footer__line'/>
+
+            {/* flex: <- space-between -> */}
+            <div className='footer__bottom footer__bottom_wrap'>
+
+                <p className='footer__year'>© 2023</p>
+                <ul className='footer__items'>
+                    <li className='footer__item'>
+                        <Link className='footer__link' to='https://practicum.yandex.ru/' target='_blank'>Яндекс.Практикум</Link>
+                    </li>
+                    <li className='footer__item'>
+                        <Link className='footer__link' to='https://github.com/ground-aero' target='_blank'>Github</Link>
+                    </li>
+                </ul>
+
+            </div>
         </footer>
     );
 }
