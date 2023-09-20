9dcc691d3fe0f8fabedbb378061dd9a95b356065
Eugene Avdeev <ea.avdeev@gmail.com>
2023-09-21 02:16:02 +0300

    style(movies card): btn type universal

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -5,20 +5,18 @@ import closeIcon from '../../../images/close_icon.svg';
 
 function MoviesCard({ type, nameRU, nameEN, image }) {
     const name = nameRU ? nameRU : nameEN;
-    // const cardBtnIcon =
+    const btnIcon = (type === 'movies') ? 'heart' : 'delete';
 
     return (
         <li className={`movies__card movies__card_${ type }`}>
             <img src={ image } className='card__img' alt='card image'/>
 
-                {/*    <p className={`card-name__wrap`}>*/}
             <span className='card__name'>{ name }
-                <div className='card__btn-wrap'>
-                    <button className='card__btn card__btn_delete'></button>
-                    {/*<img src={closeIcon} className='card__btn card__btn_close card__heart card__heart_active' alt='heart image'/>*/}
+                <div className={`card__btn-wrap card__btn-wrap_${btnIcon} `}>
+                    <button type='button' className={`card__btn card__btn_${btnIcon} `}></button>
+                    {/*<img src={closeIcon} className='card__btn card__btn_delete card__btn_heart card__heart card__heart_active' alt='heart image'/>*/}
                 </div>
             </span>
-                {/*</p>*/}
 
                 <span className='card__duration'>1ч42мин</span>
 
