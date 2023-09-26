27f343929cd93e63420e3ffbbd29892960d91033
Eugene Avdeev <ea.avdeev@gmail.com>
2023-09-27 00:07:49 +0300

    style(component/movies-card): like inactive

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -13,7 +13,7 @@ function MoviesCard({ type, nameRU, nameEN, image }) {
 
             <span className='card__name'>{ name }
                 <div className={`card__btn-wrap card__btn-wrap_${btnIcon} `}>
-                    <button type='button' className={`card__btn card__btn_${btnIcon} `}></button>
+                    <button type='button' className={`card__btn card__btn_${btnIcon} card__btn_${btnIcon}_active`}></button>
                     {/*<img src={closeIcon} className='card__btn card__btn_delete card__btn_heart card__heart card__heart_active' alt='heart image'/>*/}
                 </div>
             </span>
