bf299587f308a3149c62dbd431ed670d3ec42b4c
Eugene Avdeev <ea.avdeev@gmail.com>
2023-10-02 10:28:21 +0300

    refactor(components/profile): inputs,margins

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,39 +10,42 @@ function Profile() {
             <FormSection name={'profile'} title={'Привет, Виталий!'}
                          captionLinkEdit={'Редактировать'} captionLinkLogout={'Выйти из аккаунта'} captionLink={'/'}>
 
-                <span className='profile__inputs'>
-                    <label className='profile__input-label' htmlFor='profile-input-name'>Имя
-                <input
-                    className='profile__input'
-                    // value={email}
-                    // onChange={handleChangeName}
-                    type='text'
-                    autoFocus
-                    id='profile-input-name'
-                    name='name'
-                    placeholder='введите Ваше имя'
-                    tabIndex='1'
-                    minLength='2'
-                    required
-                />
-                    </label>
+                <div className='profile__inputs'>
 
-                    <label className='profile__input-label' htmlFor='profile-input-email'>E-mail
-                <input
-                    className='profile__input profile__input_type_email'
-                    // value={email}
-                    // onChange={handleChangeName}
-                    type='email'
-                    id='profile-input-email'
-                    placeholder='введите Ваш email'
-                    name='email'
-                    tabIndex='2'
-                    minLength='4'
-                    required
-                />
-                    </label>
+                    <span className='profile__input-wrap'>
+                        <input
+                            className='profile__input'
+                            // value={email}
+                            // onChange={handleChangeName}
+                            type='text'
+                            autoFocus
+                            id='profile-input-name'
+                            name='name'
+                            placeholder='введите Ваше имя'
+                            tabIndex='1'
+                            minLength='2'
+                            required
+                        />
+                        <label className='profile__input-label' htmlFor='profile-input-name'>Имя</label>
+                    </span>
 
-                </span>
+                    <span className='profile__input-wrap'>
+                        <input
+                            className='profile__input profile__input_type_email'
+                            // value={email}
+                            // onChange={handleChangeName}
+                            type='email'
+                            id='profile-input-email'
+                            placeholder='введите Ваш email'
+                            name='email'
+                            tabIndex='2'
+                            minLength='4'
+                            required
+                        />
+                        <label className='profile__input-label' htmlFor='profile-input-email'>E-mail</label>
+                    </span>
+
+                </div>
 
             </FormSection>
 
